fd60655aa1a05e775433ba2172b80a2d98a9a187
Adalto Selau Sparremberger <adaltoss@MacBook-Pro-de-Adalto.local>
2020-03-18 21:10:35 -0300

    Commit Inicial

---
+++ executar_fila.py
@@ -0,0 +1,17 @@
+from Fila import Fila
+
+fila = Fila()
+
+fila.imprimir()
+
+fila.adicionar( 10 )
+
+fila.adicionar( 15 )
+
+fila.adicionar( 5 )
+
+fila.remover()
+
+fila.remover()
+
+fila.remover()
